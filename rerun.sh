b2a5be61564dc8cc2e96b1fe301fc2cd99d77964
Luis Castro <luismendoncacastro@gmail.com>
2026-06-29 22:47:15 +0100

    change rerun

--- rerun.sh
+++ rerun.sh
@@ -5,4 +5,10 @@ set -x
 mkdir -p ~/recordings
 mvn install -DskipTests -Dktlint.skip
 docker build -t jitsi-multitrack-recorder:latest .
-docker run -e JMR_FINALIZE_SCRIPT=/scripts/finalize-flatten.sh -p 8989:8989 -v ~/recordings:/data jitsi-multitrack-recorder:latest
+docker run -d \
+  --name jitsi-multitrack-recorder \
+  --restart unless-stopped \
+  -e JMR_FINALIZE_SCRIPT=/scripts/finalize-flatten.sh \
+  -p 8989:8989 \
+  -v /root/recordings:/data \
+  jitsi-multitrack-recorder:latest
